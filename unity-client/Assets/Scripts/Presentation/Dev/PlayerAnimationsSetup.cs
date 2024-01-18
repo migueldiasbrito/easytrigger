f405ea441bfd32e8b152def82f23245f90bc03dc
migueldiasbrito <migueldiasbrito@gmail.com>
2024-01-18 23:05:23 +0000

    WIP Enemy animations

--- unity-client/Assets/Scripts/Presentation/Dev/PlayerAnimationsSetup.cs
+++ unity-client/Assets/Scripts/Presentation/Dev/PlayerAnimationsSetup.cs
@@ -1,6 +1,7 @@
 ﻿using Mdb.EasyTrigger.Presentation.Character;
 using Mdb.EasyTrigger.Presentation.Config;
 using Mdb.EasyTrigger.Presentation.Input;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Mdb.EasyTrigger.Presentation.Dev
@@ -10,10 +11,13 @@ namespace Mdb.EasyTrigger.Presentation.Dev
         [SerializeField] private InputController _inputController;
         [SerializeField] private CharacterInputListener _characterController;
         [SerializeField] private PlatformConfig _platformConfig;
+        [SerializeField] private List<CharacterView> _enemies;
 
         private void Start()
         {
             _characterController.Setup(_inputController, _platformConfig);
+
+            _enemies.ForEach(enemie => enemie.Setup(_platformConfig));
         }
 
         private void OnDestroy()
